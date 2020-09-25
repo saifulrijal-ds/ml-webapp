e35e44047ade2c09e16d73b1f21c20a80365d1f9
Saiful Rijal <saifulrijal.ds@gmail.com>
2020-09-25 18:40:11 +0700

    update file

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def main():
     st.sidebar.title("Binary Classification Web App")
     st.markdown("Are your mushrooms edible or poisonous? 🍄")
     st.sidebar.markdown("Are your mushrooms edible or poisonous? 🍄")
+    st.set_option('deprecation.showPyplotGlobalUse', False)
     
     @st.cache(persist=True)
     def load_data():
@@ -27,11 +28,11 @@ def main():
 
     @st.cache(persist=True)
     def split_data(df):
-        y = df['class']
-        X = df.drop(columns=['class'])
+        y = df['type']
+        X = df.drop(columns=['type'])
         # membagi data menjadi data train dan test
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
-        return X_train, X_test, y_train, y_train
+        return X_train, X_test, y_train, y_test
 
     def plot_metrics(metrics_list):
         if 'Confusion Matrix' in metrics_list:
@@ -52,7 +53,6 @@ def main():
     df = load_data()
     X_train, X_test, y_train, y_test = split_data(df)
     class_names = ['edible', 'poisonous']
-
     st.sidebar.subheader("Choose Classifier")
     # membuat dropdown menu untuk memilih classifier
     classifier = st.sidebar.selectbox("Classifier", ("Support Vector Machine (SVM)", "Logistic Regression", "Random Forest"))
@@ -60,16 +60,66 @@ def main():
     if classifier == 'Support Vector Machine (SVM)':
         st.sidebar.subheader("Model Hyperparameters")
         C = st.sidebar.number_input("C (Regulariztion Parameter)", 0.01, 10.0, step=0.01, key='C')
-        kernel = st.sidebar.radio("Kernel", ("rbf", "linear"), key='kenrel')
+        kernel = st.sidebar.radio("Kernel", ("rbf", "linear"), key='kernel')
         gamma = st.sidebar.radio("Gamma (Kernel Coefficient)", ("scale", "auto"), key='gamma')
 
-    
+        metrics = st.sidebar.multiselect("What metrics to plot?", ("Confusion Matrix", "ROC Curve", "Pecision-Recall Curve"))
+
+        if st.sidebar.button("Classify", key='classify'):
+            st.subheader("Support Vector Machine (SVM) Result")
+            model = SVC(C=C, kernel=kernel, gamma=gamma)
+            model.fit(X_train, y_train)
+            accuracy = model.score(X_test, y_test)
+            y_pred = model.predict(X_test)
+            st.write("Accuracy: ", accuracy.round(2))
+            st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
+            st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
+            plot_metrics(metrics)
+
+    if classifier == 'Logistic Regression':
+        st.sidebar.subheader("Model Hyperparameters")
+        C = st.sidebar.number_input("C (Regulariztion Parameter)", 0.01, 10.0, step=0.01, key='C_LR')
+        max_iter = st.sidebar.slider("Maximum number of iterations", 100, 500, key='max_iter')
+
+        metrics = st.sidebar.multiselect("What metrics to plot?", ("Confusion Matrix", "ROC Curve", "Pecision-Recall Curve"))
+
+        if st.sidebar.button("Classify", key='classify'):
+            st.subheader("Logistic Regression Result")
+            model = LogisticRegression(C=C, max_iter=max_iter)
+            model.fit(X_train, y_train)
+            accuracy = model.score(X_test, y_test)
+            y_pred = model.predict(X_test)
+            st.write("Accuracy: ", accuracy.round(2))
+            st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
+            st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
+            plot_metrics(metrics)
+
+    if classifier == 'Random Forest':
+        st.sidebar.subheader("Model Hyperparameters")
+        n_estimators = st.sidebar.number_input("The number of trees in the forest", 100, 5000, step=10, key='n_estimators')
+        max_depth = st.sidebar.number_input("The maximum depth of tree", 1, 20, step=1, key='max_depth')
+        bootstrap = st.sidebar.radio("Bootstrap samples when building tree", (True, False), key='bootstrap')
+
+        metrics = st.sidebar.multiselect("What metrics to plot?", ("Confusion Matrix", "ROC Curve", "Pecision-Recall Curve"))
+
+        if st.sidebar.button("Classify", key='classify'):
+            st.subheader("Random Forest Result")
+            model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, bootstrap=bootstrap, n_jobs=-1)
+            model.fit(X_train, y_train)
+            accuracy = model.score(X_test, y_test)
+            y_pred = model.predict(X_test)
+            st.write("Accuracy: ", accuracy.round(2))
+            st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
+            st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
+            plot_metrics(metrics)
+
 
     if st.sidebar.checkbox("Display raw data", False):
         st.subheader("Mushroom Dataset (Classification)")
-        st.write(df['class'].value_counts()/df['class'].count())
+        st.write(df['type'].value_counts()/df['type'].count())
         st.write(df)
 
 
 if __name__ == '__main__':
     main()
+
